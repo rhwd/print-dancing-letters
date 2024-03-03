80af18075664d2c6489ad33e21584df9bff78fa8
Kaynã de Camargo <kayna.khad@gmail.com>
2024-03-03 17:07:15 -0300

    feat: read json file into a hashmap

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::fs;
 use std::process::Command;
 
 pub fn run(arg: &String) {
@@ -28,9 +30,16 @@ pub fn run(arg: &String) {
 }
 
 fn parse_arg(arg: &String) {
+    let char_paths = parse_chars_json();
     let mut chars: Vec<u8> = Vec::new();
     for byte in arg.bytes() {
         println!("{}", byte);
         chars.push(byte)
     }
+}
+
+fn parse_chars_json() -> HashMap<u8, String> {
+    let contents = fs::read_to_string("src/chars.json").expect("failed to read file.");
+    let json: HashMap<u8, String> = serde_json::from_str(&contents).expect("Failed to parse JSON");
+    json
 }
